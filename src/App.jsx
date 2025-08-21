da408a7f2304f653cda0518dabfdebc5bb6a0a36
unknown <shakir.evan.3070@Fec.edu.bd>
2025-08-21 10:30:35 +0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Blogs from './Component/Blogs/Blogs'
+import Bookmarks from './Component/Bookmarks/Bookmarks'
+import Header from './Component/Header/Header'
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <div className='md:flex'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+
+      </div>
+    </>
+  )
+}
+
+export default App
